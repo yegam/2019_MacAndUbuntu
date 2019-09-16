21034663033d4818d6873c60ecdeb7433c26c32c
Gam <gam@Gam-MacBookPro.local>
2019-09-17 01:05:53 +0900

    update

--- db/project1.sql
+++ db/project1.sql
@@ -1,58 +1,100 @@
 #1.Grass 타입의 포켓몬의 이름을 사전순으로 출력하세요
-select name from Pokemon
-where type ='Grass'
-order by name;
+SELECT name FROM Pokemon
+WHERE type ='Grass'
+ORDER BY name;
 
 #2.Brown City나 Rainbow City 출신 트레이너의 이름을 사전순으로 출력하세요
-select name from Trainer
-where hometown ='Brown city' OR hometown = 'Rainbow City'
-order by name;
+SELECT name FROM Trainer
+WHERE hometown ='Brown city' OR hometown = 'Rainbow City'
+ORDER BY name;
 
 #3.모든 포켓몬의 type을 중복없이 사전순으로 출력하세요
-select Distinct type from Pokemon
-order by type;
+SELECT DISTINCT type FROM Pokemon
+ORDER BY type;
 
 #4.도시의 이름이 B로 시작하는 모든 도시의 이름을 사전순으로 출력하세요
-select name from City
-where name LIKE 'B%'
-order by name;
+SELECT name FROM City
+WHERE name LIKE 'B%'
+ORDER BY name;
 
 #5.이름이 M으로 시작하지 않는 트레이너의 고향을 사전순으로 출력하세요
-select hometown from Trainer
-where name NOT LIKE 'M%'
-order by hometown;
+SELECT hometown FROM Trainer
+WHERE name NOT LIKE 'M%'
+ORDER BY hometown;
 
 #6.잡힌 포켓몬 중 가장 레벨이 높은 포켓몬의 별명을 사전순으로 출력하세요
-select nickname from CatchedPokemon
-where level = (select max(level) from CatchedPokemon)
-order by nickname;
+SELECT nickname FROM CatchedPokemon
+WHERE level = (SELECT MAX(level) FROM CatchedPokemon)
+ORDER BY nickname;
 
 #7.포켓몬의 이름이 알파벳 모음으로 시작하는 포켓몬의 이름을 사전순으로 출력하세요
-select name from Pokemon
-where name Like 'a%' OR name Like 'e%' OR name Like 'i%' OR name Like 'o%' OR name Like 'u%'
-order by name;
+SELECT name FROM Pokemon
+WHERE name Like 'a%' 
+OR name Like 'e%' 
+OR name Like 'i%' 
+OR name Like 'o%' 
+OR name Like 'u%'
+ORDER BY name;
 
 #8.잡힌 포켓몬의 평균 레벨을 출력하세요
-select AVG(level) from CatchedPokemon;
+SELECT AVG(level) FROM CatchedPokemon;
 
 #9.Yellow가 잡은 포켓몬의 최대 레벨을 출력하세요
-select Max(c.level) from CatchedPokemon c
-Join Trainer t ON t.id = c.owner_id And t.name = 'yellow';
+SELECT Max(CatchedPokemon.level) 
+FROM CatchedPokemon, Trainer
+WHERE Trainer.id = CatchedPokemon.owner_id 
+And Trainer.name = 'yellow';
 
 #10.트레이너의 고향 이름을 중복없이 사전순으로 출력하세요
-select Distinct hometown from Trainer
-order by hometown;
-
+SELECT DISTINCT hometown FROM Trainer
+ORDER BY hometown;
 
 #11.닉네임이 A로 시작하는 포켓몬을 잡은 트레이너의 이름과 포켓몬의 닉네임을 트레이너의 이름의 사전순으로 출력하세요
+SELECT Trainer.name, nickname
+FROM CatchedPokemon, Trainer, Pokemon 
+WHERE nickname LIKE 'A%' 
+AND CatchedPokemon.owner_id = Trainer.id 
+AND CatchedPokemon.pid = Pokemon.id
+ORDER BY Trainer.name;
+
 #12.Amazon 특성을 가진 도시의 리더의 트레이너 이름을 출력하세요
-#13.불속성 포켓몬을 가장 많이 잡은 트레이너의 id와, 그 트레이너가 잡은 불속성 포켓몬의 수를 출력하세요
+SELECT Trainer.name
+FROM Trainer, City, Gym
+WHERE City.description = 'Amazon'
+AND Gym.city = City.name
+AND Gym.leader_id = Trainer.id;
+
+#//13.불속성 포켓몬을 가장 많이 잡은 트레이너의 id와, 그 트레이너가 잡은 불속성 포켓몬의 수를 출력하세요
+SELECT CatchedPokemon.owner_id,count(*)
+FROM CatchedPokemon, Pokemon
+WHERE CatchedPokemon.pid = Pokemon.id
+AND Pokemon.type = 'Fire';
 
 #14.포켓몬 ID가 한 자리 수인 포켓몬의 type을 중복 없이 포켓몬 ID의 내림차순으로 출력하세요
+SELECT DISTINCT type FROM Pokemon
+WHERE id < 10
+ORDER BY id DESC;
+
 #15.포켓몬의 type이 Fire이 아닌 포켓몬의 수를 출력하세요
+SELECT count(type) FROM Pokemon
+WHERE type != 'Fire';
+
 #16.진화하면 id가 작아지는 포켓몬의 진화 전 이름을 사전순으로 출력하세요
+
+
 #17.트레이너에게 잡힌 모든 물속성 포켓몬의 평균 레벨을 출력하세요
-#18.체육관리더가잡은모든포켓몬중레벨이가장높은포켓몬의별명을출력하세요
+SELECT AVG(level) 
+FROM CatchedPokemon,Pokemon
+WHERE CatchedPokemon.pid = Pokemon.id 
+AND Pokemon.type = 'Water';
+
+#18.체육관 리더가 잡은 모든 포켓몬 중 레벨이 가장 높은 포켓몬의 별명을 출력하세요
+SELECT nickname FROM CatchedPokemon, Gym
+WHERE CatchedPokemon.owner_id = Gym.leader_id
+And CatchedPokemon.level = (SELECT MAX(level) 
+                            FROM CatchedPokemon, Gym
+                            WHERE CatchedPokemon.owner_id = Gym.leader_id);
+
 #19.Blue city 출신 트레이너들 중 잡은 포켓몬들의 레벨의 평균이 가장 높은 트레이너의 이름을 사전순으로 출력하세요
 #20.같은 출신이 없는 트레이너들이 잡은 포켓몬중 진화가 가능하고 Electric 속성을 가진 포켓몬의 이름을 출력하세요
 #21.관장들의 이름과 각 관장들이 잡은 포켓몬들의 레벨 합을 레벨 합의 내림차 순으로 출력하세요
